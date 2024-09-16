dba6ec25d70fc6b4b870f22c45c92807885d5463
Eugene Egorov <egorovzhenya@gmail.com>
2024-09-16 21:20:27 +0300

    Clean up minor issues

--- src/pages/profile/index.profile.js
+++ src/pages/profile/index.profile.js
@@ -1,17 +1,9 @@
 import Handlebars from "handlebars";
 import template from './profile.hbs?raw';
+import { testProfileData } from "../../temp/fixtures";
 
 
-const fixtures = {
-    'Name': 'Homer',
-    'Last name': 'Simpson',
-    'Email': 'donut@inbox.com',
-    'Login': 'D\'oh',
-    'Chat name': 'Homer',
-    'Phone': '+7(999)1234567'
-}
-
-let profileData = Object.entries((fixtures)).map(([key, value]) => ({key, value}));
+const profileData = Object.entries((testProfileData)).map(([key, value]) => ({key, value}));
 
 export const profilePage = () => {
     const compiled = Handlebars.compile(template);
