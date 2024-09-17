6b1bd4d12df50160dae11966bb4a11902f000363
Eugene Egorov <egorovzhenya@gmail.com>
2024-09-17 11:01:38 +0300

    Register partial for a form input

--- src/components/index.partials.js
+++ src/components/index.partials.js
@@ -1,4 +1,6 @@
 import Handlebars from 'handlebars';
 import button from './button/button.hbs?raw';
+import formInput from './formInput/formInput.hbs?raw';
 
-Handlebars.registerPartial('Button', button);
+Handlebars.registerPartial('button', button);
+Handlebars.registerPartial('formInput', formInput);
