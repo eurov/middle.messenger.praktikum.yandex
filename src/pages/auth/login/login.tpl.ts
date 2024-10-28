ea1b640994e2bfb2aa3515b5b2527883bbb56cae
Eugene Egorov <egorovzhenya@gmail.com>
2024-10-28 22:49:26 +0300

    Update login template

---
+++ src/pages/auth/login/login.tpl.ts
@@ -0,0 +1,10 @@
+export default `
+    <form class="login-form card">
+        <h3>Log in</h3>
+        <div class="inputs">
+            {{{input}}}
+        </div>
+            {{{ button }}}
+    </form>`
+
+
