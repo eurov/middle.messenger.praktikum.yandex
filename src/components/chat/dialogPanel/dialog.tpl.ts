9545d7fdad9dc72de60ea62411a911c287e8cd0c
Eugene Egorov <egorovzhenya@gmail.com>
2024-11-03 17:46:15 +0300

    Update dialogPanel component

--- src/components/chat/dialogPanel/dialog.tpl.ts
+++ src/components/chat/dialogPanel/dialog.tpl.ts
@@ -1,15 +1,26 @@
 export default `
+{{#if messages}}
 <div class="chat-dialog">
     <div class="chat-dialog__header">
         <div class="chat-dialog__avatar"></div>
-            <div class="chat-dialog__name">{{name}}</div>
-            <button class="chat-dialog__menu-button">
-               {{!-- <img src="#" /> --}}
+        <div class="chat-dialog__name">{{name}}</div>
+        <button class="chat-dialog__menu-button">
+            {{!-- <img src="#" /> --}}
            </button>
-       </div>
-
+    </div>
     <div class="chat-dialog__messages">
-        {{{messageCards}}}
+        <div class="chat-dialog__date-label">June 19</div>
+        {{#each messages}}
+        <div class="chat-dialog__message{{#if this.senderIsYou}} chat-dialog__message_yours{{/if}}">
+            <div class="chat-dialog__message-text">{{this.text}}</div>
+            <div class="chat-dialog__message-time">
+                {{#if this.delivered}}
+                {{!-- <img src="#" /> --}}
+                {{/if}}
+                {{this.time}}
+            </div>
+        </div>
+        {{/each}}
     </div>
     <div class="chat-dialog__input-container">
         <input class="chat-dialog__input"
@@ -20,43 +31,9 @@ export default `
             {{!-- <img src="#" /> --}}
         </button>
     </div>
-</div>`;
-
-
-
-// <div class="chat-dialog">
-//     <div class="chat-dialog__header">
-//         <div class="chat-dialog__avatar"></div>
-//         <div class="chat-dialog__name">{{activeChat.name}}</div>
-//         <button class="chat-dialog__menu-button">
-//             {{!-- <img src="#" /> --}}
-//         </button>
-//     </div>
-
-//     <div class="chat-dialog__messages">
-//         <div class="chat-dialog__date-label">June 19</div>
-//         {{#each activeChat.messages}}
-//         <div class="chat-dialog__message{{#if this.senderIsYou}} chat-dialog__message_yours{{/if}}">
-//             <div class="chat-dialog__message-text">{{this.text}}</div>
-//             <div class="chat-dialog__message-time">
-//                 {{#if this.delivered}}
-//                 {{!-- <img src="#" /> --}}
-//                 {{/if}}
-//                 {{this.time}}
-//             </div>
-//         </div>
-//         {{/each}}
-//     </div>
-
-//     <form class="chat-dialog__input-container">
-//         <button type="button" class="chat-dialog__attach-button">
-//             {{!-- <img src="#" /> --}}
-//         </button>
-
-//         <input class="chat-dialog__input" name="message" placeholder="Message">
-
-//         <button class="chat-dialog__send" type="submit">
-//             {{!-- <img src="#" /> --}}
-//         </button>
-//     </form>
-// </div>
+</div>
+{{else}}
+<div class="container centered">
+    <div class="chat__empty-message">Select a chat to open a dialog box</div>
+</div>
+{{/if}}`;
--- src/components/chat/dialogPanel/index.dialog.ts
+++ src/components/chat/dialogPanel/index.dialog.ts
@@ -1,15 +1,9 @@
 import template from './dialog.tpl';
 import { Block } from '@utils/block';
-// import ChatInput from '../ChatInput';
-// import ChatItem from '../ChatItem';
-// import { type IChatItem } from '../ChatItem/chatItem.props';
 
 
 export default class DialogPanel extends Block {
     constructor(props) {
-        console.log('#######################')
-        console.log(props)
-        console.log('#######################')
         super({
             ...props,
             classes: 'chat__main-screen',
@@ -20,15 +14,4 @@ export default class DialogPanel extends Block {
         return this.compile(template, this.props);
     }
 }
-
-
-
-// import Handlebars from "handlebars";
-// import template from './dialog.hbs?raw';
-
-
-// export const chatDialog = (item) => {
-//     const compiled = Handlebars.compile(template)
-//     return compiled({ activeChat: item.chat });
-// }
 
