b509c71ab73ac287259fd0f70fef91e310c9ce5c
Eugene Egorov <egorovzhenya@gmail.com>
2024-11-03 15:57:21 +0300

    Keep updating render method

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,60 @@
-import './components/index.partials.js'
-import { loginPage } from './pages/auth/login/index.login.js';
-import { signupPage } from './pages/auth/signup/index.signup.js';
-import { profilePage } from './pages/profile/index.profile.js';
-import { chatsPage } from './pages/chats/index.chats.js';
-import { dialogsDispatcher } from './pages/chats/index.chats.js';
-import { notFoundPage } from './pages/errors/404/404.js';
+import LoginPage from './pages/auth/login/index.login';
+import SignupPage from './pages/auth/signup/index.signup';
+import ProfilePage from './pages/profile/index.profile';
+import ChatsPage from './pages/chats/index.chats';
+// import { notFoundPage } from './pages/errors/404/404.js';
 
 
 
 class App {
-    private page: HTMLElement;
-    private url: string;
+  private page: HTMLElement;
 
-    constructor() {
-        this.page = document.getElementById('app');
-        this.url = window.location.pathname;
+  private url: string;
+
+  constructor() {
+    this.page = document.getElementById('app');
+    this.url = window.location.pathname;
+  }
+
+  preventReboot() {
+    const pageLinks = this.page.getElementsByTagName('a');
+    for (const link of pageLinks) {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        this.url = (e.target as Element).attributes.href.textContent;
+        this.render();
+      });
     }
-    preventReboot(){
-        const pageLinks = this.page.getElementsByTagName("a")
-        for (const link of pageLinks) {
-            link.addEventListener('click', (e) => {
-                e.preventDefault();
-                this.url = e.target.attributes.href.textContent
-                this.render()
-            })
-        }
+  }
+
+  public render() {
+    switch (this.url) {
+      case '/':
+      case '/login':
+        this.page.append(new LoginPage().element);
+        break;
+      case '/signup':
+        this.page.append(new SignupPage().element);
+        break;
+      case '/profile':
+        this.page.append(new ProfilePage({ edit: false }).element);
+        break;
+      case '/profile/edit':
+        this.page.append(new ProfilePage({edit: true}).element);
+        break;
+      case '/chats':
+        this.page.append(new ChatsPage().element)
+        break;
+      default:
+        // this.page.innerHTML = notFoundPage();
+        console.log('NOT FOUND');
     }
-    public render() {
-        switch (this.url) {
-            case '/':
-            case '/login':
-                this.page.innerHTML = loginPage();
-                break
-            case '/signup':
-                this.page.innerHTML = signupPage();
-                break
-            case '/profile':
-                this.page.innerHTML = profilePage();
-                break
-            case '/profile/edit':
-                this.page.innerHTML = profilePage({edit: true});
-                break
-            case '/chats':
-                this.page.innerHTML = chatsPage();
-                document.querySelectorAll('.chat-list-item').forEach((el) => {
-                    el.addEventListener('click', dialogsDispatcher);
-                });
-                break
-            default:
-                this.page.innerHTML = notFoundPage();
-        }
-        this.preventReboot();
-    }
+    // this.preventReboot();
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const app = new App()
-    app.render()
+  const app = new App();
+  app.render();
 });
 
