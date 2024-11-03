cb2a0daa56bed5f135e12078842dda92bf74d97a
Eugene Egorov <egorovzhenya@gmail.com>
2024-11-04 01:55:06 +0300

    Resolve test failures

--- src/components/link/index.link.ts
+++ src/components/link/index.link.ts
@@ -6,6 +6,7 @@ import { Block } from '@utils/block';
 interface ILink {
     href: string,
     text: string
+    classes?: string | string[]
 }
 export default class Link extends Block<ILink> {
     constructor(props: ILink) {
