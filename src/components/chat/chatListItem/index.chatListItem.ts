deabde1339df8d909d7e3d812c0cba35428f8f9a
Eugene Egorov <egorovzhenya@gmail.com>
2024-11-03 17:47:58 +0300

    Add chatItem component

---
+++ src/components/chat/chatListItem/index.chatListItem.ts
@@ -0,0 +1,19 @@
+import template from './chatListItem.tpl';
+import { Block } from '@/utils/block';
+
+
+
+export default class chatListItem extends Block {
+    constructor(props, tagName: string = 'li') {
+        const { uuid = null } = props
+        super({
+            ...props,
+            classes: 'chat-list-item',
+            attributes: {'id': uuid}
+        }, tagName);
+    }
+
+    render(): DocumentFragment {
+        return this.compile(template, this.props);
+    }
+}
