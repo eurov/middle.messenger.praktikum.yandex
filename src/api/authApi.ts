c185cfb3bd86609e54a7e0a23b8fe58dd84c3160
Eugene Egorov <egorovzhenya@gmail.com>
2025-01-26 17:51:10 +0300

    Define AuthApi class

---
+++ src/api/authApi.ts
@@ -0,0 +1,51 @@
+import { BaseApi } from '@/api/utils/baseApi';
+
+
+export interface ISignupData {
+    first_name: string,
+    second_name: string,
+    login: string,
+    email: string,
+    password: string,
+    phone: string
+}
+
+export interface ISigninData {
+    login: string,
+    password: string
+}
+
+export interface IUser {
+    id: number,
+    first_name: string,
+    second_name: string,
+    display_name: string,
+    phone: string,
+    login: string,
+    avatar: string,
+    email: string
+}
+
+class AuthApi extends BaseApi {
+    constructor() {
+        super('/auth');
+    }
+
+    signup(data: ISignupData) {
+        return this.http.post('/signup', data);
+    }
+
+    signin(data: ISigninData) {
+        return this.http.post('/signin', data);
+    }
+
+    logout() {
+        return this.http.post('/logout');
+    }
+
+    getUser(): Promise<IUser> {
+        return this.http.get('/user');
+    }
+}
+
+export default new AuthApi();
