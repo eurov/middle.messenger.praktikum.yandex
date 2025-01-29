ca978f72fe96e08fce16318e1b54f31619589b5c
Eugene Egorov <egorovzhenya@gmail.com>
2025-01-29 22:33:00 +0300

    Update Login onSubmit event

--- src/pages/auth/login/index.login.ts
+++ src/pages/auth/login/index.login.ts
@@ -2,7 +2,9 @@ import template from './login.tpl';
 import { Block } from '@utils/block';
 import Input from '@/components/input/index.input';
 import Button from '@/components/button/index.button';
-// import { validateSignin } from '@/utils/helpers';
+import { AuthController } from '@/controllers/authController';
+import { getFormData } from '@utils/helpers';
+import { ISigninData } from '@api/authApi'
 
 
 
@@ -25,6 +27,10 @@ const button = [new Button({
 })];
 
 
+async function onSubmit(event: SubmitEvent) {
+    await AuthController.signin(getFormData(event) as ISigninData);
+}
+
 
 export default class LoginPage extends Block {
     constructor() {
@@ -35,7 +41,7 @@ export default class LoginPage extends Block {
                 button,
             },
             events: {
-                // submit: validateSignin,
+                submit: onSubmit
             },
         });
     }
