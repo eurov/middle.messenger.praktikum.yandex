2f3c0700da7cb1f56069643a8eadcf01b861db8e
Eugene Egorov <egorovzhenya@gmail.com>
2025-01-29 18:51:55 +0300

    Define app entrypoint

--- src/main.ts
+++ src/main.ts
@@ -1,68 +1,118 @@
-import LoginPage from './pages/auth/login/index.login';
-import SignupPage from './pages/auth/signup/index.signup';
-import ProfilePage from './pages/profile/index.profile';
-import ChatsPage from './pages/chats/index.chats';
+import { router } from '@/utils/router';
+import { AuthController } from '@controllers/authController';
+
+import LoginPage from '@/pages/auth/login/index.login';
+import SignupPage from '@/pages/auth/signup/index.signup';
+import ChatsPage from '@/pages/chats/index.chats';
+import ProfilePage from '@/pages/profile/index.profile';
 import NotFoundPage from './pages/errors/404/404.js';
 
 
-
-class App {
-  private page: HTMLElement;
-
-  url: string;
-
-  constructor(baseElement: HTMLElement) {
-    this.page = baseElement;
-    this.url = window.location.pathname;
-  }
-
-  preventReboot() {
-    const pageLinks = this.page.getElementsByTagName('a');
-    for (const link of pageLinks) {
-      link.addEventListener('click', (e) => {
-        e.preventDefault();
-        const nextPage = (e.target as Element).getAttribute('href');
-        if (nextPage) {
-          this.url = nextPage;
-          this.render();
-        }
-      });
-    }
-  }
-
-  public render() {
-    this.page.innerHTML = '';
-    switch (this.url) {
-      case '/':
-      case '/login':
-        this.page.append(new LoginPage().element);
-        break;
-      case '/signup':
-        this.page.append(new SignupPage().element);
-        break;
-      case '/profile':
-        this.page.append(new ProfilePage({ edit: false }).element);
-        break;
-      case '/profile/edit':
-        this.page.append(new ProfilePage({ edit: true }).element);
-        break;
-      case '/chats':
-        this.page.append(new ChatsPage().element);
-        break;
-      default:
-          this.page.append(new NotFoundPage().element);
-    }
-    this.preventReboot();
-  }
+export enum Routes {
+  Index = '/',
+  Login = '/login',
+  SignUp = '/sign-up',
+  Chats = '/messenger',
+  Profile = '/profile',
+  ChangePassword = '/change-password',
+  ChangeData = '/settings',
+  NotFound = '/404',
 }
 
-export let app: App;
+window.addEventListener('DOMContentLoaded', async () => {
+  router
+    .use(Routes.Index, LoginPage)
+    .use(Routes.Login, LoginPage)
+    .use(Routes.SignUp, SignupPage)
+    .use(Routes.Chats, ChatsPage)
+    .use(Routes.Profile, ProfilePage)
+    // .use(Routes.ChangePassword, ChangePassword)
+    // .use(Routes.ChangeData, ChangeData)
+    .use(Routes.NotFound, NotFoundPage)
+    .start()
 
-document.addEventListener('DOMContentLoaded', () => {
-  const baseElement = document.getElementById('app');
-  if (baseElement) {
-    app = new App(baseElement);
-    app.render();
+  let isProtected = true;
+  switch (window.location.pathname) {
+    case Routes.Index:
+    case Routes.Login:
+    case Routes.SignUp:
+      isProtected = false;
+      break;
+  }
+
+  try {
+    await AuthController.fetchUser();
+    if (!isProtected) {
+      router.go(Routes.Chats);
+    }
+  } catch (_) {
+    if (isProtected) {
+      router.go(Routes.Login);
+    }
   }
 });
+
+
+
+
+
+// class App {
+//   private page: HTMLElement;
+
+//   url: string;
+
+//   constructor(baseElement: HTMLElement) {
+//     this.page = baseElement;
+//     this.url = window.location.pathname;
+//   }
+
+//   preventReboot() {
+//     const pageLinks = this.page.getElementsByTagName('a');
+//     for (const link of pageLinks) {
+//       link.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         const nextPage = (e.target as Element).getAttribute('href');
+//         if (nextPage) {
+//           this.url = nextPage;
+//           this.render();
+//         }
+//       });
+//     }
+//   }
+
+//   public render() {
+//     this.page.innerHTML = '';
+//     switch (this.url) {
+//       case '/':
+//       case '/login':
+//         this.page.append(new LoginPage().element);
+//         break;
+//       case '/signup':
+//         this.page.append(new SignupPage().element);
+//         break;
+//       case '/profile':
+//         this.page.append(new ProfilePage({ edit: false }).element);
+//         break;
+//       case '/profile/edit':
+//         this.page.append(new ProfilePage({ edit: true }).element);
+//         break;
+//       case '/chats':
+//         this.page.append(new ChatsPage().element);
+//         break;
+//       default:
+//           this.page.append(new NotFoundPage().element);
+//     }
+//     this.preventReboot();
+//   }
+// }
+
+// export let app: App;
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const baseElement = document.getElementById('app');
+//   if (baseElement) {
+//     app = new App(baseElement);
+//     app.render();
+//   }
+// });
 
