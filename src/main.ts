d8ee7dfdccbca1fbf53fa54642bc1065fdf30375
Eugene Egorov <egorovzhenya@gmail.com>
2025-02-03 21:14:56 +0300

    Define main module

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { router } from '@/utils/router';
 import { AuthController } from '@controllers/authController';
-
 import LoginPage from '@/pages/auth/login/index.login';
 import SignupPage from '@/pages/auth/signup/index.signup';
 import ChatsPage from '@/pages/chats/index.chats';
-import ProfilePage from '@/pages/profile/index.profile';
+import { ProfilePage } from '@/pages/profile/card/index.profile.js';
+import { ChangeDataPage } from '@/pages/profile/changeData/index.changeData.js';
+import { ChangePasswordPage } from '@/pages/profile/changePassword/index.changePassword.js';
 import NotFoundPage from './pages/errors/404/404.js';
 
 
@@ -14,8 +15,8 @@ export enum Routes {
   SignUp = '/sign-up',
   Chats = '/messenger',
   Profile = '/profile',
-  ChangePassword = '/change-password',
-  ChangeData = '/settings',
+  ChangeData = '/profile/edit',
+  ChangePassword = '/profile/password_edit',
   NotFound = '/404',
 }
 
@@ -26,10 +27,9 @@ window.addEventListener('DOMContentLoaded', async () => {
     .use(Routes.SignUp, SignupPage)
     .use(Routes.Chats, ChatsPage)
     .use(Routes.Profile, ProfilePage)
-    // .use(Routes.ChangePassword, ChangePassword)
-    // .use(Routes.ChangeData, ChangeData)
+    .use(Routes.ChangeData, ChangeDataPage)
+    .use(Routes.ChangePassword, ChangePasswordPage)
     .use(Routes.NotFound, NotFoundPage)
-    .start()
 
   let isProtected = true;
   switch (window.location.pathname) {
@@ -42,77 +42,14 @@ window.addEventListener('DOMContentLoaded', async () => {
 
   try {
     await AuthController.fetchUser();
+    router.start();
     if (!isProtected) {
       router.go(Routes.Chats);
     }
   } catch (_) {
+    router.start();
     if (isProtected) {
-      router.go(Routes.Login);
+      router.go(Routes.Index);
     }
   }
 });
-
-
-
-
-
-// class App {
-//   private page: HTMLElement;
-
-//   url: string;
-
-//   constructor(baseElement: HTMLElement) {
-//     this.page = baseElement;
-//     this.url = window.location.pathname;
-//   }
-
-//   preventReboot() {
-//     const pageLinks = this.page.getElementsByTagName('a');
-//     for (const link of pageLinks) {
-//       link.addEventListener('click', (e) => {
-//         e.preventDefault();
-//         const nextPage = (e.target as Element).getAttribute('href');
-//         if (nextPage) {
-//           this.url = nextPage;
-//           this.render();
-//         }
-//       });
-//     }
-//   }
-
-//   public render() {
-//     this.page.innerHTML = '';
-//     switch (this.url) {
-//       case '/':
-//       case '/login':
-//         this.page.append(new LoginPage().element);
-//         break;
-//       case '/signup':
-//         this.page.append(new SignupPage().element);
-//         break;
-//       case '/profile':
-//         this.page.append(new ProfilePage({ edit: false }).element);
-//         break;
-//       case '/profile/edit':
-//         this.page.append(new ProfilePage({ edit: true }).element);
-//         break;
-//       case '/chats':
-//         this.page.append(new ChatsPage().element);
-//         break;
-//       default:
-//           this.page.append(new NotFoundPage().element);
-//     }
-//     this.preventReboot();
-//   }
-// }
-
-// export let app: App;
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const baseElement = document.getElementById('app');
-//   if (baseElement) {
-//     app = new App(baseElement);
-//     app.render();
-//   }
-// });
-
