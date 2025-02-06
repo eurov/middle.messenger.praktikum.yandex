795a89e8f69cb8f512e2f739a6fff6bd4a79211c
Eugene Egorov <egorovzhenya@gmail.com>
2025-02-06 22:42:34 +0300

    Add chats controller

---
+++ src/controllers/chatsController.ts
@@ -0,0 +1,88 @@
+import { store } from '@utils/store';
+import chatsApi from '@api/chatApi';
+import MessagesController from './messageController';
+
+
+export class ChatController {
+    static async getUsers(id: number) {
+        try {
+            const data = await chatsApi.getChatUsers(id);
+            store.set('users', data);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    static async getChats() {
+        try {
+            const chats = await chatsApi.getChats({});
+            chats.map(async (chat) => {
+                const token = await this.getToken(chat.id);
+                if (token) await MessagesController.connect(chat.id, token);
+            });
+
+            chats.map((chat) => {
+                if (chat?.last_message?.time) {
+                    chat.last_message.time = `${new Date(
+                        chat.last_message.time,
+                    ).toLocaleTimeString()}`;
+                }
+                return chat;
+            });
+            store.set('chats', chats);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    static async create(title: string) {
+        try {
+            await chatsApi.createChat(title);
+            this.getChats();
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    static async getToken(id: number) {
+        try {
+            const { token } = await chatsApi.getChatTokens(id);
+            return token;
+        } catch (e) {
+            console.error(e);
+            return undefined;
+        }
+    }
+
+    static async selectCurrentChat(id: string) {
+        store.set('currentChat', +id);
+    }
+
+    static async addUserToChat(chatId: number, userId: number) {
+        try {
+            await chatsApi.addChatUsers([userId], chatId);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    static async deleteUserToChat(chatId: number, userId: number) {
+        try {
+            await chatsApi.deleteChatUsers([userId], chatId);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    static async uploadChatAvatar(data: File, chatId: number) {
+        try {
+            const result = await chatsApi.uploadChatAvatar(chatId, data);
+            store.set('chats', store.getState().chats.map((chat) => {
+                if (chat.id === result.id) return result;
+                return chat;
+            }));
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
