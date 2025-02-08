793adc5425a1cb3e0dfbe1a003ba041f398a9a70
Eugene Egorov <egorovzhenya@gmail.com>
2025-02-08 19:36:35 +0300

    Update chat item component

--- src/components/chat/chatListItem/index.chatListItem.ts
+++ src/components/chat/chatListItem/index.chatListItem.ts
@@ -1,28 +1,39 @@
 import template from './chatListItem.tpl';
 import { Block } from '@/utils/block';
+import { withStore, IState } from '@/utils/store';
+import { ElementProps } from '@utils/block';
+import { ChatController } from '@controllers/chatsController';
 
 
-interface IChatListItem {
-    uuid: string,
-    name: string,
-    massages?: ({
-        senderIsYou: boolean;
-        time: string;
-        text: string;
-        unread?: boolean;
-    })
-}
-export default class ChatListItem extends Block<IChatListItem> {
-    constructor(props: IChatListItem, tagName: string = 'li') {
-        const { uuid } = props;
+const getChatId = (element: EventTarget | null): string | null => {
+    if (!element) return null;
+    const chatId = (element as HTMLElement).getAttribute('data-chat');
+    return chatId || getChatId((element as HTMLElement).parentElement);
+};
+
+class ChatItem extends Block {
+    constructor(props: ElementProps) {
         super({
             ...props,
-            classes: 'chat-list-item',
-            attributes: { 'id': uuid },
-        }, tagName);
+            classes: 'chat__chats-list',
+            events: {
+                click: (e: Event) => {
+                    const chatId = getChatId(e.target);
+                    if (chatId) ChatController.selectCurrentChat(chatId);
+                },
+            },
+        }, 'ul');
     }
 
     render(): DocumentFragment {
         return this.compile(template, this.props);
     }
 }
+
+const mapStateToProps = (state: IState) => {
+    return {
+        chatsList: state.chats,
+    };
+};
+
+export const ChatItemWithStore = withStore(mapStateToProps)(ChatItem);
